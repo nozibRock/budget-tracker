e05e1ee7d3762e372d115e93b2d5e48c6cff9645
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-17 16:42:20 +0600

    add savings funcitonalities

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ function updateTotalExpenses() {
   const rentExpense = parseFloat(getValue("rentExpensesInput"));
   const clothExpense = parseFloat(getValue("clothesExpensesInput"));
 
-  return (foodExpense + rentExpense + clothExpense);
+  /* if (foodExpense > 0 && rentExpense > 0 && clothExpense > 0) {
+      
+  } */
+  return foodExpense + rentExpense + clothExpense;
 }
 function balance() {
   const income = parseFloat(getValue("incomeInput"));
@@ -16,11 +19,25 @@ function balance() {
   return income - totalExpenses;
 }
 
+function savingsAmount() {
+  const balanceAmount = parseFloat(balance());
+  const perc = document.getElementById("percentage");
+  const percentage = parseFloat(perc.value);
+  return ((balanceAmount * percentage) / 100).toFixed(3);
+}
 window.addEventListener("load", () => {
   document.getElementById("calculate-btn").addEventListener("click", () => {
     const totalExpenses = document.getElementById("totalExpenses");
     totalExpenses.innerText = updateTotalExpenses();
     const balanceText = document.getElementById("balance");
     balanceText.innerText = balance();
+  });
+  document.getElementById("save").addEventListener('click', () => {
+      const savings = document.getElementById("saving-amount");
+        const savingsText = savingsAmount();
+      savings.innerText = savingsText;
+      const balanceAmount = balance();
+      const remainBalance = document.getElementById("remaining-balance");
+      remainBalance.innerText = parseFloat(balanceAmount) - parseFloat(savingsText)
   });
 });
